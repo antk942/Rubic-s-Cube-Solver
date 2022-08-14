5d3bc47a4f77cf8415f00ed49d9864791f9d79b0
Κωνσταντίνος Αντωνιάδης <antk942@gmail.com>
2022-08-14 14:11:30 +0300

    Fixed interior colors of cube

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -11,8 +11,11 @@ public class Cube : MonoBehaviour
     public GameObject leftSide;
     public GameObject rightSide;
 
+    public Vector3 vec3_1 = new Vector3();
+    public Vector3 vec3_2 = new Vector3();
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         frontSide.GetComponent<Renderer>().material.color = Color.white;
         backSide.GetComponent<Renderer>().material.color = Color.yellow;
@@ -21,4 +24,38 @@ public class Cube : MonoBehaviour
         leftSide.GetComponent<Renderer>().material.color = Color.green;
         rightSide.GetComponent<Renderer>().material.color = Color.blue;
     }
+    public List<GameObject> GetSidesInsideCube(float insideCubeLength)
+    {
+        List<GameObject> sides = GetSides();
+        List<GameObject> insideSides = new List<GameObject>();
+
+        float leniency = 0f;
+        vec3_1 = new Vector3(-leniency, -leniency, -leniency);
+        vec3_2 = new Vector3(insideCubeLength + leniency, insideCubeLength + leniency, insideCubeLength + leniency);
+
+        foreach (var side in sides)
+        {            
+            if(side.transform.position.x >= -leniency && side.transform.position.x <= insideCubeLength + leniency &&
+               side.transform.position.y >= -leniency && side.transform.position.y <= insideCubeLength + leniency &&
+               side.transform.position.z >= -leniency && side.transform.position.z <= insideCubeLength + leniency)
+            {
+                insideSides.Add(side);
+            }
+        }
+        return insideSides;
+    }
+    private List<GameObject> GetSides()
+    {
+        return new List<GameObject>() { frontSide, backSide, downSide, upSide, leftSide, rightSide };
+    }
+    void OnDrawGizmos()
+    {
+        float centerL = (vec3_2.x - vec3_1.x) / 2;
+        Vector3 vec3_3 = new Vector3(centerL, centerL, centerL);
+
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(vec3_3 + vec3_1, vec3_2);
+    }
 }
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public GameObject cube;
 
-    public int cubeLenght = 3;
+    public int rubicsSize = 3;
     public float inBetweenOffSet = 0.5f;
 
     // Start is called before the first frame update
@@ -25,20 +25,28 @@ public class GameManager : MonoBehaviour
     {
         GameObject rubicsCube = new GameObject("Rubic's Cube");
 
-        float cubeOffset = cube.GetComponentInChildren<Renderer>().bounds.size.x + inBetweenOffSet;
+        float planeLength = cube.GetComponentInChildren<Renderer>().bounds.size.x;
+        float cubeOffset = planeLength + inBetweenOffSet;
+        float rubicsLength = planeLength * rubicsSize + inBetweenOffSet * (rubicsSize - 1);
 
-        for (int i = 0; i < cubeLenght; i++)
+        for (int i = 0; i < rubicsSize; i++)
         {
-            for (int k = 0; k < cubeLenght; k++)
+            for (int k = 0; k < rubicsSize; k++)
             {
-                for (int l = 0; l < cubeLenght; l++)
+                for (int l = 0; l < rubicsSize; l++)
                 {
                     Vector3 position = new Vector3(i * cubeOffset, k * cubeOffset, l * cubeOffset);
                     GameObject newCube = Instantiate(cube, position, Quaternion.identity);
                     newCube.name = $"{i} {k} {l}";
                     newCube.transform.parent = rubicsCube.transform;
+
+                    List<GameObject> closestSides = newCube.GetComponent<Cube>().GetSidesInsideCube(rubicsLength - planeLength);
+                    foreach (GameObject side in closestSides)
+                    {
+                        side.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+                    }
                 }
             }
         }
-    }
+    }    
 }
